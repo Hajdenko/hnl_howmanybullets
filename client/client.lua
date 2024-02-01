12bbeaedcf95e1c267bd4b5506ad2efd3fe7a24e
Hajden <138399356+Hajdenko@users.noreply.github.com>
2024-02-01 23:58:34 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,11 +1,17 @@
 CreateThread(function()
     while true do
+
         local Player = PlayerPedId()
         local WeaponHash = GetSelectedPedWeapon(Player)
 
+        local sleep = 100        
         local ammo = 0
 
-        if(IsPedArmed(Player, 4)) then _, ammo = GetAmmoInClip(Player, WeaponHash) end
+        if (IsPedArmed(Player, 4)) then 
+            _, ammo = GetAmmoInClip(Player, WeaponHash)
+            sleep = 100
+        else sleep = 500
+        end
 
         SendNUIMessage({
             action = "Status",
@@ -13,6 +19,6 @@ CreateThread(function()
             Armed = IsPedArmed(Player, 4),
         })
 
-        Wait(100)
+        Wait(sleep)
     end
 end)
